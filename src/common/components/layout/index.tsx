18e869528538cdc4b3de4f055b08aa9b3bfcb919
Helen Trapeznikova <strelchenya-91@mail.ru>
2022-07-03 22:18:56 +0300

    added store

--- src/common/components/layout/index.tsx
+++ src/common/components/layout/index.tsx
@@ -1,22 +1,30 @@
 import React from "react";
 import {Route, Routes} from "react-router-dom";
+import {useSelector} from "react-redux";
 
+import store from "@store/index";
 import Header from "@components/header";
 import Home from "@pages/home";
 import About from "@pages/about";
 
 import styles from "./style.module.scss";
 
-const Layout = () => (
-    <>
-        <Header/>
-        <main>
-            <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/about" element={<About/>}/>
-            </Routes>
-        </main>
-    </>
-);
+const Layout = () => {
+    const store = useSelector(store => store);
+
+    console.log(store);
+
+    return (
+        <>
+            <Header/>
+            <main>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/about" element={<About/>}/>
+                </Routes>
+            </main>
+        </>
+    );
+}
 
 export default Layout;
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {Provider} from 'react-redux';
 import {BrowserRouter} from "react-router-dom";
 import Layout from "@components/layout";
+import store from "@store/index";
 
 ReactDOM.render(
   <React.StrictMode>
-      <BrowserRouter>
-          <Layout />
-      </BrowserRouter>
+      <Provider store={store}>
+          <BrowserRouter>
+              <Layout />
+          </BrowserRouter>
+      </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
